f6f513e735dbc7c01395e0cc812d0e2a8707a7dd
LowDuck <Duckjacobsen@gmail.com>
2021-06-01 20:12:42 +0200

    commit #mistakes

--- main.py
+++ main.py
@@ -3,23 +3,42 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivymd.app import MDApp
 from kivy.core.window import Window
-
+from kivy.properties import ObjectProperty
 #Uix import
 from kivy.uix.behaviors import TouchRippleBehavior
 from kivy.uix.button import Button
 from kivy.uix.widget import Widget
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.boxlayout import BoxLayout
+
 
 Window.size = (350,600)
 
-class MainScreen(Screen):
+
+
+class HomeScreen(Screen):
     pass
 
-class FirstScreen(Screen):
+class LoginScreen(Screen):
     pass
+
+class SignupScreen(Screen):
+    pass
+
+class HomeScreen(Screen):
+    pass
+
+class SettingScreen(Screen):
+    pass
+
 
 class ScreenManager(ScreenManager):
     pass
+
+
+class ContentNavigationDrawer(BoxLayout):
+    screen_manager = ObjectProperty()
+    nav_drawer = ObjectProperty()
 
 class MainApp(MDApp):
     def build(self):
