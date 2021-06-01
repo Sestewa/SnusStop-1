2971a4c29642faae69f8ed216dcd117ad17868d0
LowDuck <duckjacobsen@gmail.com>
2021-06-02 00:11:37 +0200

    commit for tmr

--- main.py
+++ main.py
@@ -3,18 +3,19 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivymd.app import MDApp
 from kivy.core.window import Window
-from kivy.properties import ObjectProperty
+from kivy.properties import ObjectProperty, StringProperty
+from kivymd.theming import ThemableBehavior
+
 #Uix import
 from kivy.uix.behaviors import TouchRippleBehavior
 from kivy.uix.button import Button
 from kivy.uix.widget import Widget
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.boxlayout import BoxLayout
+from kivymd.uix.list import OneLineIconListItem, MDList
 
 
 Window.size = (350,600)
-
-
 
 class HomeScreen(Screen):
     pass
@@ -31,7 +32,6 @@ class HomeScreen(Screen):
 class SettingScreen(Screen):
     pass
 
-
 class ScreenManager(ScreenManager):
     pass
 
@@ -40,6 +40,35 @@ class ContentNavigationDrawer(BoxLayout):
     screen_manager = ObjectProperty()
     nav_drawer = ObjectProperty()
 
+class ItemDrawer(OneLineIconListItem):
+    icon = StringProperty()
+
+class DrawerList(ThemableBehavior, MDList):
+    def set_color_item(self, instance_item):
+        '''Called when tap on a menu item.'''
+
+        # Set the color of the icon and text for the menu item.
+        for item in self.children:
+            if item.text_color == self.theme_cls.primary_color:
+                item.text_color = self.theme_cls.text_color
+                break
+        instance_item.text_color = self.theme_cls.primary_color
+
+    def on_start(self):
+        icons_item = {
+        "folder": "My files",
+        "account-multiple": "Shared with me",
+        "star": "Starred",
+        "history": "Recent",
+        "checkbox-marked": "Shared with me",
+        "upload": "Upload",
+        }
+        for icon_name in icons_item.keys():
+            self.root.ids.content_drawer.ids.md_list.add_widget(
+                ItemDrawer(icon=icon_name, text=icons_item[icon_name])
+            )
+
+
 class MainApp(MDApp):
     def build(self):
         return Builder.load_file('AppDesign.kv')
@@ -47,5 +76,7 @@ class MainApp(MDApp):
     def change_screen(self, screen, direction):
         self.root.transition.direction = direction
         self.root.current = screen
+
+
 
 MainApp().run()
